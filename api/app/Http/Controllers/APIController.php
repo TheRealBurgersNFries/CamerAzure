49403bb584f28ece3a1a73b037e600711706e42a
Jon Tan <jaeteemail@gmail.com>
2015-11-21 23:09:09 -0500

    Testing api

--- api/app/Http/Controllers/APIController.php
+++ api/app/Http/Controllers/APIController.php
@@ -7,9 +7,11 @@ use Illuminate\Routing\Controller as BaseController;
 abstract class APIController extends BaseController
 {
     public function requestHandler($request_name){
-    	return response()->json([
-    								"success" => 1,
-    								"request" => $request_name
-    								]);
+    	$response = array("success" => 1, 
+    					  "request" => $request_name);
+    	return Response::json($response);
+    }
+    public function index($request_name){
+    	return response()->view('api')->with($request_name);
     }
 }
