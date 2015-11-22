a59909f5e3d0c4a3503e679db8bf035635058e39
Jon Tan <jaeteemail@gmail.com>
2015-11-22 12:28:49 -0500

    demo

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -11,9 +11,14 @@
 |
 */
 
+Route::get('demo', function () {
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('beats', function () {
     return view('welcome');
